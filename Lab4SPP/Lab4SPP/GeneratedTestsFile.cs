83f37ee1e5f1264a907a1d832dbfcbe0b9024c1e
VladislavYaKo <korsunov_2000@mail.ru>
2020-12-04 15:51:48 +0300

    Pipeline was formed in method Generate(), except delegate in getTest

---
+++ Lab4SPP/Lab4SPP/GeneratedTestsFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4SPP
+{
+    public class GeneratedTestsFile
+    {
+        public string fileName;
+        public string contentText;
+
+        public GeneratedTestsFile(string fName, string content)
+        {
+            fileName = fName;
+            contentText = content;
+        }
+    }
+}
---
+++ Lab4SPP/Lab4SPP/Generator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Lab4SPP
+{
+    public class Generator
+    {
+        private List<string> _filesForTest;
+        private string _testFolder;
+        private int _maxThreads;
+        private int _maxFilesToRead;
+        private int _maxFilesToWrite;
+
+        public Generator(List<string> files, string folder, int maxThreads, int maxFilesToRead, int maxFilesToWrite)
+        {
+            _filesForTest = files;
+            _testFolder = folder;
+            _maxThreads = maxThreads;
+            _maxFilesToRead = maxFilesToRead;
+            _maxFilesToWrite = maxFilesToWrite;
+        }
+
+        public async Task GenerateAsync()
+        {
+            var readFile = new TransformBlock<string, string>(async path =>
+            {
+                string fileContent;
+                using (StreamReader reader = File.OpenText(path))
+                {
+                    fileContent = await reader.ReadToEndAsync();
+                }
+                return fileContent;
+
+            }, new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = _maxFilesToRead
+            });
+
+            var getTest = new TransformBlock<string, List<GeneratedTestsFile>>(text =>
+            {
+                //TODO
+                return new List<GeneratedTestsFile>();
+            }, new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = _maxThreads
+            });
+
+            var writeFile = new ActionBlock<List<GeneratedTestsFile>>(async testFiles =>
+            {
+                foreach (var testFile in testFiles)
+                {
+                    using (StreamWriter writer = File.CreateText(testFile.fileName))
+                    {
+                        await writer.WriteAsync(testFile.contentText);
+                    }
+                }
+            }, new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = _maxFilesToWrite
+            });
+
+            var pipelineOptions = new DataflowLinkOptions { PropagateCompletion = true };
+
+            readFile.LinkTo(getTest, pipelineOptions);
+            getTest.LinkTo(writeFile, pipelineOptions);
+            foreach(string file in _filesForTest)
+            {
+                readFile.Post(file);
+            }
+
+            readFile.Complete();
+
+            await writeFile.Completion;
+        }
+
+    }
+}
